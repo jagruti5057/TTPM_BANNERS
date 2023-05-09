29a9aba2243ef8fc8db030a558d778e326855cce
jagruti <jagruti.patil.5057@gmail.com>
2023-05-09 15:12:38 +0530

    vercel changes 4

--- banner_apis/settings.py
+++ banner_apis/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-6)py!p(r_i+fbgvkczic6#jv62(6dziijisy3mb&1ehyal48f('
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY="django-insecure-6)py!p(r_i+fbgvkczic6#jv62(6dziijisy3mb&1ehyal48f("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,11 +127,11 @@ DATABASES = {
 # EMAIL_HOST_PASSWORD = "tebkwwnuzwzauhbj"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = int(env('EMAIL_PORT'))
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "abhijit.umbarkar.5057@gmail.com"
+EMAIL_HOST_PASSWORD = "tebkwwnuzwzauhbj
 # DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 
 # DEFAULT_FROM_EMAIL = 'your_email_address'
